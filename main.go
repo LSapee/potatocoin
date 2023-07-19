7840a40c87d9ef5339c5b6f6321645ab9c033dcd
LSapee <dlworhks94@gmail.com>
2023-07-19 13:26:55 +0900

    go web server 열기

--- main.go
+++ main.go
@@ -2,19 +2,17 @@ package main
 
 import (
 	"fmt"
-	"github.com/LSapee/potatocoin/blockchain"
+	"log"
+	"net/http"
 )
 
-func main() {
-	chain := blockchain.GetBlockchain()
-	chain.AddBlock("감자 블록")
-	chain.AddBlock("감자1 블록3")
-	chain.AddBlock("감자2 블록2")
-	chain.AddBlock("감자3 블록1")
-	for _, block := range chain.AllBlocks() {
-		fmt.Println("Data : ", block.Data)
-		fmt.Println("Hash : ", block.Hash)
-		fmt.Println("PrevHash : ", block.PrevHash)
-	}
+const port string = ":4000"
 
+func home(rw http.ResponseWriter, request *http.Request) {
+	fmt.Fprint(rw, "hello from home")
+}
+
+func main() {
+	http.HandleFunc("/", home)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
