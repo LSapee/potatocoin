1e09b3372d38eedd9515c116d2de4469546e1131
LSapee <dlworhks94@gmail.com>
2023-08-21 20:08:16 +0900

    flag를 이용하여 CLI 만들기

---
+++ cli/cli.go
@@ -0,0 +1,42 @@
+package cli
+
+import (
+	"flag"
+	"fmt"
+	"github.com/LSapee/potatocoin/explorer"
+	"github.com/LSapee/potatocoin/rest"
+	"os"
+)
+
+func usage() {
+	fmt.Printf("Welcome to 감자 코인\n\n")
+	fmt.Printf("Please use the follwing flags:\n\n")
+	fmt.Printf("-port=4000: 		Set the PORT of hte server\n")
+	fmt.Printf("-mode=rest:		Choose between 'html' and 'rest'\n")
+	os.Exit(0)
+}
+
+func Start() {
+
+	if len(os.Args) == 1 {
+		usage()
+	}
+	//기본값이 필요함
+	port := flag.Int("port", 4000, "Set port of the server")
+	mode := flag.String("mode", "rest", "Choose between 'html' and 'rest'")
+
+	flag.Parse()
+
+	switch *mode {
+	case "rest":
+		//start rest api
+		rest.Start(*port)
+	case "html":
+		//start html
+		explorer.Start(*port)
+	default:
+		usage()
+	}
+
+	fmt.Println(*port, *mode)
+}
