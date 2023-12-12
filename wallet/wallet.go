4d705f0a9af6313d6e4f24d623eff0443a1b5c22
LSapee <dlworhks94@gmail.com>
2023-12-13 01:56:32 +0900

    WALLETS 키페어 생성 및 검증 과정

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -1,25 +1,32 @@
 package wallet
 
-import (
-	"crypto/ecdsa"
-	"crypto/elliptic"
-	"crypto/rand"
-	"encoding/hex"
-	"fmt"
-	"github.com/LSapee/potatocoin/utils"
+// hashedMessage , privateKey,signature은 모두 다른 곳에서 불러와서 각자 다른 방법으로 복구를 함
+const (
+	hashedMessage string = "c33084feaa65adbbbebd0c9bf292a26ffc6dea97b170d88e501ab4865591aafd"
+	privateKey    string = "30770201010420110fa1076b89ed41dd993d10ffb40a54dc00fd2720c2c770e6524713068349a4a00a06082a8648ce3d030107a14403420004315997a9a2116c76462a423cd33b351be2a9aa9e11e9aeb0215f903b9b0c433255ea5db56154a26a811183dd049e6c3a526a95e5f9e48f9282c0b690c82aecac"
+	signature     string = "e08cf51a25c9608d8a83c31f45deecd5334f2e6bf08600c14bbf639937f53729a3ffe96327063d73472c52ae33c70393456261ff93dbd65ca6e06cb1c6523c47"
 )
 
 func Start() {
-	//ecdsa GO포함된 패키지 (Ellipc Curve Digital Signature Algorithm)
-	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-	utils.HandleErr(err)
-	//1. 메시지 생성
-	message := "I love you"
-	hashedMessage := utils.Hash(message)
-	// Hash값이 String이라 다시 []byte값으로 변환
-	hashAsBytes, err := hex.DecodeString(hashedMessage)
-	utils.HandleErr(err)
-	r, s, err := ecdsa.Sign(rand.Reader, privateKey, hashAsBytes)
-	utils.HandleErr(err)
-	fmt.Printf("R:%d\nS:%d", r, s)
+
+	////ecdsa GO포함된 패키지 (Ellipc Curve Digital Signature Algorithm)
+	//privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	//
+	//keyAsByes, err := x509.MarshalECPrivateKey(privateKey)
+	//fmt.Printf("%x\n\n\n", keyAsByes)
+	//
+	//utils.HandleErr(err)
+	//// Hash값이 String이라 다시 []byte값으로 변환
+	//hashAsBytes, err := hex.DecodeString(hashedMessage)
+	//utils.HandleErr(err)
+	//r, s, err := ecdsa.Sign(rand.Reader, privateKey, hashAsBytes)
+	//signature := append(r.Bytes(), s.Bytes()...)
+	//fmt.Printf("%x\n", signature)
+	//utils.HandleErr(err)
+	////fmt.Printf("R:%d\nS:%d\n", r, s)
+	//
+	////검증
+	////ok := ecdsa.Verify(&privateKey.PublicKey, hashAsBytes, r, s)
+	////
+	////fmt.Println(ok)
 }
