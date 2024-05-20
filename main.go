ec7948551465b723953a89649015dc8b213de384
LSapee <dlworhks94@gmail.com>
2024-05-20 15:58:28 +0900

    채널 만들기 닫기

--- main.go
+++ main.go
@@ -1,12 +1,38 @@
 package main
 
 import (
-	"github.com/LSapee/potatocoin/cli"
-	"github.com/LSapee/potatocoin/db"
+	"fmt"
+	"time"
 )
 
+// chan<- or <-chan이 아니라 그냥 c chan int 하면 보내기,받기 둘다 가능함
+
+// 채널로 값을 보내기 전용
+func countToTen(c chan<- int) {
+	for i := range [10]int{} {
+		time.Sleep(1 * time.Second)
+		c <- i
+	}
+	close(c)
+}
+
+// 채널로 값을 받기 정용
+func receive(c <-chan int) {
+	for {
+		a, ok := <-c
+		if !ok {
+			break
+		}
+		fmt.Println(a)
+	}
+}
+
 func main() {
+	// 채널 만들기
+	c := make(chan int)
+	go countToTen(c)
+	receive(c)
 	//wallet.Wallet()
-	defer db.Close()
-	cli.Start()
+	//defer db.Close()
+	//cli.Start()
 }
